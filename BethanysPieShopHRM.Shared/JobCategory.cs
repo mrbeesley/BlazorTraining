408ac7d3c34f5363470a7526e58d0e0b0220aa94
Michael Beesley <mrbeesley@users.noreply.github.com>
2020-11-12 14:18:16 -0600

    initial commit

---
+++ BethanysPieShopHRM.Shared/JobCategory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace BethanysPieShopHRM.Shared
+{
+    public class JobCategory
+    {
+        public int JobCategoryId { get; set; }
+        public string JobCategoryName { get; set; }
+    }
+}
